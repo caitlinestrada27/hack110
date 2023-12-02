6216954b90e4c6e789e8d363b66ba82f4b800fca
Caitlin Estrada <100039736+caitlinestrada23@users.noreply.github.com>
2023-12-01 21:05:28 -0500

    adding create todo page

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app: Flask = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/create-todo')
+def create_todo():
+    return render_template("create-todo.html")
  
 if __name__ == '__main__':
     app.run(debug=True)

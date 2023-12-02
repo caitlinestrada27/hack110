4e8ef296a2add09d8bee4d5172e073b47759db1c
Parker Stiles <pbstiles@unc.edu>
2023-12-02 01:14:45 -0500

    hack110 progress

--- data/data_wrangling.py
+++ data/data_wrangling.py
@@ -7,12 +7,11 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
     """Read CSV file and return dict."""
     output: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
-    csv_reader: DictReader = DictReader(file_handle)
+    csv_reader = DictReader(file_handle)
     for row in csv_reader:
         output.append(row)
     file_handle.close()
     return output
 
-filename: str = "MCU_Timeline.csv"
-x = read_csv_rows(filename)
-print(x)
+filename: str = "../MCU_timeline.csv"
+print(read_csv_rows(filename))

91945049b8ea134e666b662470b4225f0776776f
Parker Stiles <pbstiles@unc.edu>
2023-12-01 21:03:45 -0500

    `

--- project/app.py
+++ project/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from helpers import todo
 
 app: Flask = Flask(__name__)
+todo_list: list[todo] = []
+todo_count: int = 0
 
 @app.route("/")
 def index():
--- project/helpers.py
+++ project/helpers.py
@@ -8,8 +8,7 @@ class todo:
 	description: str
 	 
 	def __init__(self, id: int, title: str, description: str):
-	    self.id = id
+		"""Todo's constructor."""
+		self.id = id
 		self.title = title
 		self.description = description
-	
-		

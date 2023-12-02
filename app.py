bfb3ea802b0845536afa227fdf05fb96ebccb2ca
Caitlin Estrada <100039736+caitlinestrada23@users.noreply.github.com>
2023-12-01 21:17:14 -0500

    adding success page

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template
 
 app: Flask = Flask(__name__)
 
+from flask import Flask, render_template, request
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/create-todo')
+@app.route('/create-todo', methods=["GET", "POST"])
 def create_todo():
+    if request.method == "POST":
+        return render_template("success.html")
+    if request.method == "GET":
+        return render_template("create-todo.html")
     return render_template("create-todo.html")
  
 if __name__ == '__main__':
